fbe2bb5aa9b7cb1533056d6372dc45616be969e4
uo <uozalp@gmail.com>
2024-05-26 23:21:00 +0300

    Adding Json retrofit

--- app/src/main/java/com/lszlp/nobec/MainActivity.java
+++ app/src/main/java/com/lszlp/nobec/MainActivity.java
@@ -2,10 +2,25 @@ package com.lszlp.nobec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.lszlp.nobec.ui.pharmacyOnDutyList.PharmacyAPI;
+import com.lszlp.nobec.ui.pharmacyOnDutyList.PharmacyModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +29,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
--- app/src/main/java/com/lszlp/nobec/ui/pharmacyOnDutyList/PharmacyOnDutyListFragment.java
+++ app/src/main/java/com/lszlp/nobec/ui/pharmacyOnDutyList/PharmacyOnDutyListFragment.java
@@ -13,6 +13,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.lszlp.nobec.MainActivity;
 import com.lszlp.nobec.databinding.FragmentDashboardBinding;
 
 import java.util.ArrayList;
@@ -27,10 +28,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class PharmacyOnDutyListFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
-
     ArrayList<PharmacyModel> pharmacyModels;
-    private String  BASE_URL ="https://www.nosyapi.com/apiv2/service/";
+    private String  BASE_URL ="https://github.com/lszlp76/jsons/tree/main/";
     Retrofit retrofit;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         PharmacyOnDutyListViewModel pharmacyOnDutyListViewModel =
@@ -42,49 +43,53 @@ public class PharmacyOnDutyListFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         pharmacyOnDutyListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Gson gson = new GsonBuilder().setLenient().create(); // create GSON
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))// Factory converts  JSON to GSON
                 .build()
         ;
-        LoadData();
-        return root;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
         super.onViewCreated(view, savedInstanceState);
     }
-private void  LoadData(){
-        PharmacyAPI  pharmacyAPI = retrofit.create(PharmacyAPI.class); // create service
-            Call<List<PharmacyModel>> call = pharmacyAPI.getData();
-            call.enqueue(new Callback<List<PharmacyModel>>() {
-                @Override
-                public void onResponse(Call<List<PharmacyModel>> call, Response<List<PharmacyModel>> response) {
-                     if (response.isSuccessful()){
-                         List<PharmacyModel> responseList = response.body();
-                         pharmacyModels = new ArrayList<>(responseList);
 
-                         for ( PharmacyModel pharmacyModel: pharmacyModels){
-                             System.out.println(pharmacyModel);
-                         }
-                     }
-                }
-
-                @Override
-                public void onFailure(Call<List<PharmacyModel>> call, Throwable t) {
-                        t.printStackTrace();
-                }
-            });
-
-}
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+    private void  LoadData(){
+        PharmacyAPI pharmacyAPI = retrofit.create(PharmacyAPI.class); // create service
+        Call<List<PharmacyModel>> call = pharmacyAPI.getData();
+        call.enqueue(new Callback<List<PharmacyModel>>() {
+            @Override
+            public void onResponse(Call<List<PharmacyModel>> call, Response<List<PharmacyModel>> response) {
+                if (response.isSuccessful()){
+                    List<PharmacyModel> responseList = response.body();
+                    pharmacyModels = new ArrayList<>(responseList);
+
+                    for ( PharmacyModel pharmacyModel: pharmacyModels){
+                        System.out.println(pharmacyModel);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PharmacyModel>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+    }
+
 }
 
 /*
