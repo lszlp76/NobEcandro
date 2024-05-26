21d9b4247adcf783c402abc4c8003ac29e8ff4bd
uo <uozalp@gmail.com>
2024-05-26 23:03:48 +0300

    Adding Json retrofit

--- app/src/main/java/com/lszlp/nobec/ui/pharmacyOnDutyList/PharmacyOnDutyListFragment.java
+++ app/src/main/java/com/lszlp/nobec/ui/pharmacyOnDutyList/PharmacyOnDutyListFragment.java
@@ -7,31 +7,86 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.lszlp.nobec.databinding.FragmentDashboardBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PharmacyOnDutyListFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
+    ArrayList<PharmacyModel> pharmacyModels;
+    private String  BASE_URL ="https://www.nosyapi.com/apiv2/service/";
+    Retrofit retrofit;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         PharmacyOnDutyListViewModel pharmacyOnDutyListViewModel =
                 new ViewModelProvider(this).get(PharmacyOnDutyListViewModel.class);
+
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
         pharmacyOnDutyListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Gson gson = new GsonBuilder().setLenient().create(); // create GSON
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))// Factory converts  JSON to GSON
+                .build()
+        ;
+        LoadData();
         return root;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+private void  LoadData(){
+        PharmacyAPI  pharmacyAPI = retrofit.create(PharmacyAPI.class); // create service
+            Call<List<PharmacyModel>> call = pharmacyAPI.getData();
+            call.enqueue(new Callback<List<PharmacyModel>>() {
+                @Override
+                public void onResponse(Call<List<PharmacyModel>> call, Response<List<PharmacyModel>> response) {
+                     if (response.isSuccessful()){
+                         List<PharmacyModel> responseList = response.body();
+                         pharmacyModels = new ArrayList<>(responseList);
+
+                         for ( PharmacyModel pharmacyModel: pharmacyModels){
+                             System.out.println(pharmacyModel);
+                         }
+                     }
+                }
+
+                @Override
+                public void onFailure(Call<List<PharmacyModel>> call, Throwable t) {
+                        t.printStackTrace();
+                }
+            });
+
+}
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
 }
+
+/*
+https://www.nosyapi.com/apiv2/pharmacyLink?apikey=x58j5AeEFXqvorUFG7X3o5QzGXqcThj8ncFGre0nucdVQYsuZKgfZ8ZKUf8y&city=bursa&county=mudanya
+ */
